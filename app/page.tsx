51f6a10c541d32e5af8ec12fbab350d56671e8cc
Tomasz Koprowski <tomazi.dev@gmail.com>
2024-11-24 22:59:26 +0000

    feat: adding some base components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const HomePage = () => {
-  return <div className="text-3xl">Home Page</div>;
+  return (
+    <div>
+      <h1 className="text-3xl">Home Page</h1>
+      <Button variant="outline" size="lg" className="capitalize m-8">
+        click me
+      </Button>
+    </div>
+  );
 };
 
 export default HomePage;
